4c631490793f06421dc33bc79abd80d0dcff9d11
Vinicius Fiuza <135707902+BearHakai@users.noreply.github.com>
2024-11-04 13:02:08 -0300

    Atualizacao de codigo
    Adicao da variavel do total a pagar

--- listaJS/lojaTintas1.js
+++ listaJS/lojaTintas1.js
@@ -2,14 +2,17 @@
 var metros = parseFloat(prompt("Informe quantos metros serão pintados: "));
 var litros = metros / 3;
 var latas = litros / 18;
+var latasPreco;
 
 //Verifica se as latas sao um numero inteiro ou real, caso seja um numero inteiro ira adicionar uma lata a mais ja que nao se compra lata pela metade
 if(!Number.isInteger(latas)){
     latas = parseInt(latas) + 1;
-    alert("Para pintar " + metros + " m², precisará de " + latas + " latas");
-    console.log("Para pintar " + metros + " m², precisará de " + latas + " latas");
+    latasPreco = latas * 80;
+    alert("Para pintar " + metros + " m², precisará de " + latas + " latas" + "\nO total a pagar será: R$ " + latasPreco);
+    console.log("Para pintar " + metros + " m², precisará de " + latas + " latas" + "\nO total a pagar será: R$ " + latasPreco);
 }
 else{
-    alert("Para pintar " + metros + " m², precisará de " + latas + " latas");
-    console.log("Para pintar " + metros + " m², precisará de " + latas + " latas");
+    latasPreco = latas * 80;
+    alert("Para pintar " + metros + " m², precisará de " + latas + " latas" + "\nO total a pagar será: R$ " + latasPreco);
+    console.log("Para pintar " + metros + " m², precisará de " + latas + " latas" + "\nO total a pagar será: R$ " + latasPreco);
 }
